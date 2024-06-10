075f72c4a63c0d15b86ae39b9685cb485ef3acae
rushichavan18 <157720099+rushichavan18@users.noreply.github.com>
2024-06-10 18:41:13 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+//alert (3 + 3)
+
+let name = "Rushi"
+let age = 18
+let isLoggedIn = false
+let state; 
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true / false
+// null => standlone value
+// undefined 
+// symbol => unique
+
+// object
+
+console.log(typeof null); // object
