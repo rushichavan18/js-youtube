dafa4ff25fe1af24e4b1b172a9a3013d98e78c48
rushichavan18 <157720099+rushichavan18@users.noreply.github.com>
2024-06-11 07:40:47 +0000

    conversion

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,20 @@
+const accountId = 144552
+let accountEmail ="rushi@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+let accountState ;
+
+//accountId = 2 // not allowed
+
+accountEmail = "hello@gmail.com"
+accountPassword = "121212"
+accountCity = "thane"
+
+console.log(accountId);
+
+/*
+prefer not to use var
+because of issude of blok code
+*/
+
+console.table( [accountId, accountEmail,accountPassword, accountCity, accountState])
