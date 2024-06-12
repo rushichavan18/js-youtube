430e011e7a68f8c5afac3c656704712eb04e97ae
rushichavan18 <157720099+rushichavan18@users.noreply.github.com>
2024-06-12 19:55:10 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,17 @@
+const name = "rushi"
+const repoCount = 50
+
+//console.log(name + repoCount + "value");
+
+console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`)
+
+
+const gameName = new String('rushichavan')
+
+console.log(gameName[0]);
+
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.indexOf('v'));
